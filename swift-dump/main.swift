998395310f8875b149ce8e96a187126baa305e81
Terence Tan <terencetanwj@gmail.com>
2016-07-24 17:41:56 +0800

    Initial Commit

---
+++ swift-dump/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-dump
+//
+//  Created by Terence Tan on 24/7/16.
+//  Copyright © 2016 KJCracks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
